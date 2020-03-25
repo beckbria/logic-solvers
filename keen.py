3e3f480c4ffab8d52e0f8d9f47882ae3a4725df4
Brian Beck <brian.beck@outlook.com>
2020-03-25 01:10:03 -0700

    Initial keen solver

---
+++ keen.py
@@ -0,0 +1,94 @@
+from z3 import *
+from z3util import *
+
+# Future improvements:
+# Assert that each square is added to only a single constraint
+# Assert that every square is added to a constraint
+
+class Keen:
+    __prefix = 'keen'
+
+    """Solver for the Keen logic puzzle: https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/keen.html"""
+    def __init__(self, size):
+        """Creates empty square puzzle.  size: Size of the square"""
+        self.size = size
+        self.grid = Z3IntDict2D(size, size, self.__prefix)
+        self.solver = Solver()
+        self.__addNumericRangeConstraints()
+        self.__addUniquenessConstraints()
+
+    def Solution(self):
+        """Solves the grid and returns a 2D array of the values"""
+        self.solver.check()
+        m = self.solver.model()
+        answer = [[0] * self.size for i in range(self.size)]
+        for y in range(self.size):
+            for x in range(self.size):
+                answer[y][x] = int(str(m[self.grid[(x,y)]]))
+        return answer
+
+    def AddSum(self, target, *coordinatesList):
+        """
+        Adds a sum constraint to the grid.
+
+        squares: List of (x,y) tuples
+        target: The sum of those squares
+        """
+        self.solver.add(Sum(self.__squares(coordinatesList)) == target)
+
+    def AddProduct(self, target, *coordinatesList):
+        """
+        Adds a product constraint to the grid.
+
+        squares: List of (x,y) tuples
+        target: The sum of those squares
+        """
+        self.solver.add(Product(self.__squares(coordinatesList)) == target)
+
+    def AddDifference(self, target, first, second):
+        """
+        Adds a subtraction constraint to the grid.
+
+        first, second: (x,y) tuples containing coordinates of the squares
+        """
+        sq = self.__squares([first, second])
+        self.solver.add(Or(sq[0]-sq[1] == target, sq[1]-sq[0] == target))
+
+    def AddDivision(self, target, first, second):
+        """
+        Adds a division constraint to the grid.
+
+        first, second: (x,y) tuples containing coordinates of the squares
+        """
+        sq = self.__squares([first, second])
+        self.solver.add(Or(sq[0]*target == sq[1], sq[1]*target == sq[0]))
+
+    def __squares(self, coordinatesList):
+        """Given a list of (x,y) tuples, returns the grid squares corresponding to them"""
+        return list(map(lambda c: self.grid[c], coordinatesList))
+
+    def __addNumericRangeConstraints(self):
+        """Ensures that all grid squares are in the range [1,size]"""
+        for key in self.grid.keys():
+            self.solver.add(Or([self.grid[key] == j for j in range(1, self.size + 1)]))
+
+    def __addUniquenessConstraints(self):
+        """Ensures that all rows and columns contain distinct values"""
+        row_c = [Distinct([self.grid[(j,i)] for j in range(self.size)]) for i in range(self.size)]
+        col_c = [Distinct([self.grid[(i,j)] for i in range(self.size)]) for j in range(self.size)]
+        self.solver.add(row_c + col_c)
+
+k = Keen(5)
+k.AddDivision(2, (0,0), (0,1))
+k.AddDivision(2, (1,2), (1,3))
+k.AddDivision(2, (3,3), (4,3))
+k.AddProduct(12, (1,1), (2,1))
+k.AddProduct(5, (3,2), (4,2))
+k.AddProduct(12, (0,2), (0,3))
+k.AddDifference(3, (1,0), (2,0))
+k.AddDifference(2, (3,0), (3,1))
+k.AddDifference(1, (0,4), (1,4))
+k.AddSum(5, (4,0), (4,1))
+k.AddSum(9, (2,2), (2,3), (2,4))
+k.AddSum(5, (3,4), (4,4))
+print(k.Solution())
---
+++ z3util.py
@@ -0,0 +1,46 @@
+"""Reusable helper functions for Z3 python wrappers"""
+from z3 import *
+
+def IsOdd(i):
+    """Returns a Z3 condition for if an Int is odd"""
+    return i % 2 != 0
+
+def IsEven(i):
+    """Returns a Z3 condition for if an Int is even"""
+    return i % 2 == 0
+
+def NumberFromDigits(*args):
+    """Given an array of Int digits, returns an Int as if they were read left-to-right"""
+    num = 0
+    for arg in args:
+        num = (10 * num) + arg
+    return num
+
+def Z3IntArray(size, prefix):
+    """Creates an array of new Z3 Int objects.  They will be named prefix0-prefix[size-1]"""
+    arr = []
+    fmt = prefix + '{num:02d}'
+    for i in range(size):
+        arr.append(Int(fmt.format(num=i)))
+    return arr
+
+def Z3IntDict2D(width, height, prefix):
+    """
+    Returns a dict keyed on (X,Y) tuples of new Z3 Int objects.  They will be named
+    prefix-X-Y.  The upper-left corner is (0,0) and the grid proceeds down and right
+    from there.
+
+    width: Width of the grid (int)
+    height: Height of the grid (int)
+    prefix: Unique string prefix to identify the Z3 variables
+    """
+    d = {}
+    for x in range(width):
+        for y in range(height):
+            d[(x,y)] = Int(Z3IntDictKey(x,y,prefix))
+    return d
+
+def Z3IntDictKey(x, y, prefix):
+    """Returns the Z3 variable name for a grid space created by Z3IntDict2D"""
+    fmt = prefix + '-{}-{}'
+    return fmt.format(x,y)
